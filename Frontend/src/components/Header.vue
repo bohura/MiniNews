e9642c6d3ae134c26258904287ccacb6bc2e7f5a
bohura <109013235+bohura@users.noreply.github.com>
2025-07-09 19:02:27 +0900

    Update Header.vue

--- Frontend/src/components/Header.vue
+++ Frontend/src/components/Header.vue
@@ -1,45 +1,41 @@
 <template>
   <div class="headerContainer">
-    <!-- 头部左侧区域 -->
+    <!-- ヘッダーの左エリア -->
     <div class="left">
       <ul>
-        <li @click="HighlightHandler(index,)"  v-for="(item,index) in findAllTypeList" :key="item.tid">
+        <li @click="HighlightHandler(index)" v-for="(item,index) in findAllTypeList" :key="item.tid">
           <a :class="{ active: item.isHighlight }" href="javascript:;">{{item.tname}}</a>
         </li>
       </ul>
     </div>
-    <!-- 头部右侧区域 -->
+    <!-- ヘッダーの右エリア -->
     <div class="right">
       <div class="rightInput" style="margin-right: 50px;">
-        <el-input v-model="keywords" placeholder="搜索最新头条"></el-input>
-        <!-- <el-button   type="primary">搜索</el-button> -->
+        <el-input v-model="keywords" placeholder="最新ニュースを検索"></el-input>
       </div>
 
-  
-      <!-- 用户登录以后的展示 -->
+      <!-- ログイン済みの表示 -->
       <div class="btn-dropdown">
-      <!-- 用户没有登录的时候的展示 -->
-     
-      <div v-if="nickName" style="display: flex; justify-content: center; align-items: center;">
-             <el-dropdown>
-          <el-button type="primary">
-          您好:{{ nickName }}<el-icon class="el-icon--right"><arrow-down /></el-icon>
-          </el-button>
-          <template #dropdown>
-            <el-dropdown-menu>
-              <el-dropdown-item @click="handlerNews">发布新闻</el-dropdown-item>
-              <el-dropdown-item>个人中心</el-dropdown-item>
-              <el-dropdown-item>浏览记录</el-dropdown-item>
-              <el-dropdown-item @click="Logout">退出登录</el-dropdown-item>
-            </el-dropdown-menu>
-          </template>
-        </el-dropdown>
-      </div>
-       <div v-else class="containerButton">
-          <el-button size="small" style="background: #212529; color: #aea7a2" @click="toLogin">登录</el-button>
-          <el-button size="small" style="background: #ffc107; color: #684802" @click="toRegister">注册</el-button>
+        <!-- 未ログイン時の表示 -->
+        <div v-if="nickName" style="display: flex; justify-content: center; align-items: center;">
+          <el-dropdown>
+            <el-button type="primary">
+              こんにちは:{{ nickName }}<el-icon class="el-icon--right"><arrow-down /></el-icon>
+            </el-button>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item @click="handlerNews">ニュースを投稿する</el-dropdown-item>
+                <el-dropdown-item>マイページ</el-dropdown-item>
+                <el-dropdown-item>閲覧履歴</el-dropdown-item>
+                <el-dropdown-item @click="Logout">ログアウト</el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
         </div>
-      
+        <div v-else class="containerButton">
+          <el-button size="small" style="background: #212529; color: #aea7a2" @click="toLogin">ログイン</el-button>
+          <el-button size="small" style="background: #ffc107; color: #684802" @click="toRegister">新規登録</el-button>
+        </div>
       </div>
     </div>
   </div>
@@ -54,47 +50,53 @@ export default defineComponent({
 
 <script setup>
 import { getfindAllTypes, isUserOverdue } from '../api/index'
-import { ref, onMounted , getCurrentInstance ,watch, onUpdated} from "vue"
+import { ref, onMounted , getCurrentInstance ,watch, onUpdated } from "vue"
 import { useRouter } from 'vue-router'
 import { ArrowDown } from '@element-plus/icons-vue'
 import { removeToken } from '../utils/token-utils' 
 import pinia from '../stores/index';
 import { useUserInfoStore } from '../stores/userInfo'
+
 const userInfoStore = useUserInfoStore(pinia)
 const nickName = ref("")
-// 获取到 全局事件总线
 const { Bus } = getCurrentInstance().appContext.config.globalProperties
 const router = useRouter()
-const keywords = ref("") // 收集搜索最新头条参数
-//监视搜索参数的变化 ,当搜索参数变化的时候给HeadlineNews组件传递数据
+const keywords = ref("") // 最新ニュース検索のキーワード
+// 検索キーワードを監視し、HeadlineNewsコンポーネントへ送信
 watch(keywords, (newVal) => {
   Bus.emit('keyword', newVal)
 })
-const findAllTypeList = ref([])//所有头条分类
+
+const findAllTypeList = ref([]) // 全てのカテゴリを取得
+
+// ログインページへ遷移
 const toLogin = () => {
-router.push({ name: "Login" });
+  router.push({ name: "Login" });
 }
-//点击去注册页面
+
+// 新規登録ページへ移動
 const toRegister = () => {
   router.push({ name: "Register" });
 }
+
+// カテゴリ一覧を取得して表示
 const getList = async () => {
   let result = await getfindAllTypes()
-  // 遍历数据添加高亮标识
   result.forEach((item) => {
     item.tid = item.tid
     item.tname = item.tname
     item.isHighlight = false
   })
-  // 添加微头条数据
+  // 「ミニニュース」を先頭に追加
   result.unshift({
     isHighlight: true,
     tid: 0,
-    tname: "微头条"
+    tname: "ミニニュース"
   })
   findAllTypeList.value = result
 }
-// 页面挂载的生命周期回调
+
+// ページマウント時の処理
 onUpdated(() => {
   nickName.value = userInfoStore.nickName
 })
@@ -102,17 +104,16 @@ onMounted(() => {
   getList()
 })
 
-//点击切换高亮的回调(排他思想)
+// ハイライト切り替え処理（排他）
 const HighlightHandler = (index) => {
   findAllTypeList.value.forEach((item) => {
     item.isHighlight = false
   })
-  // 切换高亮的时候把tid传给HeadlineNews组件
   findAllTypeList.value[index].isHighlight = true
   Bus.emit('tid', findAllTypeList.value[index].tid)
 }
 
-// 点击退出登录的回调
+// ログアウト処理
 const Logout = () => {
   removeToken()
   userInfoStore.initUserInfo()
@@ -120,9 +121,8 @@ const Logout = () => {
   router.go({ name: "HeadlineNews" });
 }
 
-//点击发布新闻的回调
+// ニュース投稿ボタンの処理
 const handlerNews = async () => {
-  //发送请求判断用户是否token过期
   await isUserOverdue()
   router.push({ name: "addOrModifyNews" });
 }
@@ -133,11 +133,9 @@ const handlerNews = async () => {
   vertical-align: top;
   width: 100px;
 }
-
 .el-dropdown+.el-dropdown {
   margin-left: 15px;
 }
-
 .el-icon-arrow-down {
   font-size: 12px;
 }
@@ -176,13 +174,13 @@ const handlerNews = async () => {
     flex-wrap: nowrap;
     .rightInput {
       display: flex;
-       align-items: center;
+      align-items: center;
       :deep(.el-input__inner) {
         height: 30px;
         width: 150px;
       }
     }
-    .btn-dropdown{
+    .btn-dropdown {
       display: flex;
       align-items: center;
     }
@@ -194,8 +192,6 @@ const handlerNews = async () => {
     }
   }
 }
-
-
 .example-showcase .el-dropdown + .el-dropdown {
   margin-left: 15px;
 }
@@ -206,21 +202,3 @@ const handlerNews = async () => {
   align-items: center;
 }
 </style>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
